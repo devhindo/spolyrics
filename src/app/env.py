1134d2fbcba27beeedcbc44f6fcb8e1e7d2c172b
devhindo <lildevhind@gmail.com>
2023-10-13 12:22:23 +0300

    init spotify app

---
+++ src/app/env.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access environment variables
+CLIENT_ID = os.environ.get('CLIENT_ID')
---
+++ src/app/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+t = "ahmed"
+l = tk.Label(root, text=t)
+
+l.pack()    
+
+root.mainloop()
